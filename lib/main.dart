49a39a1a926f0c1916d8c263f2daee34fcc6d0c7
Moein Alkozay <moein.alkozay@microcis.net>
2023-05-06 19:09:39 +0430

    Add the phone number container

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,24 @@ class Card extends StatelessWidget {
                       fontFamily: 'pacifico',
                       letterSpacing: 2),
                 ),
+                Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(6),
+                  margin:
+                  const EdgeInsets.symmetric(vertical: 30, horizontal: 70),
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('+93793061612')
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
